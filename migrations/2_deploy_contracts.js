cbacfa0a4d5c8144695b61d7230fdcc5f6a66b67
Nuno Machado <nmiguelmoura@gmail.com>
2019-01-13 12:26:11 +0000

    type: feat Add Ownable contract to inherit all others.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,6 +5,7 @@ var DistributorRole = artifacts.require("./DistributorRole.sol");
 var RetailerRole = artifacts.require("./RetailerRole.sol");
 var ConsumerRole = artifacts.require("./ConsumerRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
+var Ownable = artifacts.require("./Ownable.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(FarmerRole);
@@ -13,4 +14,5 @@ module.exports = function(deployer) {
     deployer.deploy(RetailerRole);
     deployer.deploy(ConsumerRole);
     deployer.deploy(SupplyChain);
+    deployer.deploy(Ownable);
 };
--- test/SupplyChain.js
+++ test/SupplyChain.js
@@ -1,4 +1,4 @@
-const SupplyChain = artifacts.require('SupplyChain');
+const SupplyChain = artifacts.require('Ownable');
 
 const STATE = {
     HARVESTED:          0,
